f14286ef1bd2fca1eeb9e762f6b101cea3ac3309
therealilyas <ilyas.sultanov2020@gmail.com>
2024-10-26 00:18:13 +0500

    add search modal

--- src/js/main.js
+++ src/js/main.js
@@ -2,23 +2,23 @@ import { categories } from "../../data/categories";
 import { products } from "../../data/products";
 
 const hideEl = (el, display = "block") => {
-    if (display) {
-      el.classList.remove(display);
-    }
-  
-    el.classList.add("hidden");
-  };
-  
-  const showEl = (el, display = "block") => {
-    el.classList.remove("hidden");
-  
-    if (display) {
-      el.classList.add(display);
-    }
-  };
-  
-  const getCateogryTemplate = (category, idx) => {
-    return `
+  if (display) {
+    el.classList.remove(display);
+  }
+
+  el.classList.add("hidden");
+};
+
+const showEl = (el, display = "block") => {
+  el.classList.remove("hidden");
+
+  if (display) {
+    el.classList.add(display);
+  }
+};
+
+const getCateogryTemplate = (category, idx) => {
+  return `
     <div class="group">
       <div
         class="category transition-all duration-200 rounded-xl border border-grey-4 group-hover:bg-blue-100 group-hover:text-white group-hover:border-white/20 flex items-center gap-3 shadow-category py-[22px] pr-3 bg-white cursor-pointer"
@@ -37,80 +37,79 @@ const hideEl = (el, display = "block") => {
         </div>
         <i class="icon-chevron rotate-[-90deg]"></i>   </div> </div>
     `;
-  };
-  
-  const renderCatetories = () => {
-    const categoriesSection = document.querySelector("section.categories");
-  
-    categories.forEach((category, idx) => {
-      categoriesSection.innerHTML = categoriesSection.innerHTML +  getCateogryTemplate(category, idx);
-    });
-  };
-  
-  renderCatetories();
-  
-  const getSubcategoryTemplate = (subcategory) => {
-    return `
+};
+
+const renderCatetories = () => {
+  const categoriesSection = document.querySelector("section.categories");
+
+  categories.forEach((category, idx) => {
+    categoriesSection.innerHTML =
+      categoriesSection.innerHTML + getCateogryTemplate(category, idx);
+  });
+};
+
+renderCatetories();
+
+const getSubcategoryTemplate = (subcategory) => {
+  return `
       <a href="#" class="flex items-center justify-between font-semibold  gap-3 p-3 hover:text-blue-100 hover:font-bold transition duration-200 ">
         <h4>${subcategory.name}</h4>
         <i class="icon-chevron rotate-[-90deg]"></i>
       </a>
     `;
-  };
-  
-  let activeIdx = null;
-  
-  const adjustDropdownPosition = (categoryIdx, dropdownContainer) => {
-    const activeRow = Math.floor(categoryIdx / 3) + 2;
-    dropdownContainer.style.gridRow = activeRow;
-  };
-  
-  const toggleDropdownVisibility = (categoryIdx, dropdownContainer) => {
-    if (dropdownContainer.classList.contains("hidden")) {
-      showEl(dropdownContainer, "grid");
-    } else if (activeIdx === categoryIdx) {
-      hideEl(dropdownContainer, "grid");
-    }
-  };
-  
-  const renderSubcategories = (subcategories, dropdownContainer) => {
-    dropdownContainer.innerHTML = subcategories
-      ? subcategories.map(getSubcategoryTemplate).join("")
-      : "Empty";
-  };
-  
-  const toggleCategoryActive = (categoryIdx) => {
-    const categories = document.querySelectorAll("section.categories .category");
-  
-    categories.forEach((c) => c.classList.remove("active"));
-  
-    if (activeIdx === categoryIdx) {
-      categories[categoryIdx].classList.remove("active");  
-    } else {
-      categories[categoryIdx].classList.toggle("active");
-    }
-  };
-  
-  window.toggleDropdown = (categoryIdx) => {
-    const subcategories = categories[categoryIdx].subcategories
-  
-    const dropdownContainer = document.querySelector(".subcategories-dropdown");
-  
-    adjustDropdownPosition(categoryIdx, dropdownContainer);
-  
-    toggleDropdownVisibility(categoryIdx, dropdownContainer);
-  
-    toggleCategoryActive(categoryIdx);
-  
-    renderSubcategories(subcategories, dropdownContainer);
-  
-    activeIdx = categoryIdx;
-  };
+};
 
+let activeIdx = null;
 
+const adjustDropdownPosition = (categoryIdx, dropdownContainer) => {
+  const activeRow = Math.floor(categoryIdx / 3) + 2;
+  dropdownContainer.style.gridRow = activeRow;
+};
+
+const toggleDropdownVisibility = (categoryIdx, dropdownContainer) => {
+  if (dropdownContainer.classList.contains("hidden")) {
+    showEl(dropdownContainer, "grid");
+  } else if (activeIdx === categoryIdx) {
+    hideEl(dropdownContainer, "grid");
+  }
+};
+
+const renderSubcategories = (subcategories, dropdownContainer) => {
+  dropdownContainer.innerHTML = subcategories
+    ? subcategories.map(getSubcategoryTemplate).join("")
+    : "Empty";
+};
+
+const toggleCategoryActive = (categoryIdx) => {
+  const categories = document.querySelectorAll("section.categories .category");
+
+  categories.forEach((c) => c.classList.remove("active"));
+
+  if (activeIdx === categoryIdx) {
+    categories[categoryIdx].classList.remove("active");
+  } else {
+    categories[categoryIdx].classList.toggle("active");
+  }
+};
+
+window.toggleDropdown = (categoryIdx) => {
+  const subcategories = categories[categoryIdx].subcategories;
+
+  const dropdownContainer = document.querySelector(".subcategories-dropdown");
+
+  adjustDropdownPosition(categoryIdx, dropdownContainer);
+
+  toggleDropdownVisibility(categoryIdx, dropdownContainer);
+
+  toggleCategoryActive(categoryIdx);
+
+  renderSubcategories(subcategories, dropdownContainer);
+
+  activeIdx = categoryIdx;
+};
 
 function createProductCard(product) {
-    return `
+  return `
     
          <div class="w-full h-full bg-white rounded-lg shadow-md mt-10 group ">
 
@@ -147,39 +146,62 @@ ${product.name}
 </div>
 </div>
     `;
-  }
-  const productGrid = document.getElementById("product-grid");
+}
+const productGrid = document.getElementById("product-grid");
 
-  products.forEach((product) => {
-    productGrid.innerHTML += createProductCard(product);
+products.forEach((product) => {
+  productGrid.innerHTML += createProductCard(product);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const languageButton = document.getElementById("language-button");
+  const languageDropdown = document.getElementById("language-dropdown");
+  const languageFlag = document.getElementById("language-flag");
+
+  languageButton.addEventListener("click", function () {
+    languageDropdown.classList.toggle("show");
   });
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const languageButton = document.getElementById('language-button');
-    const languageDropdown = document.getElementById('language-dropdown');
-    const languageFlag = document.getElementById('language-flag'); 
-  
-    languageButton.addEventListener('click', function () {
-      languageDropdown.classList.toggle('show');
-    });
-  
-    languageDropdown.addEventListener('click', function (event) {
-      if (event.target.tagName === 'LI') {
-        const selectedLanguage = event.target.getAttribute('data-lang');
-        const selectedFlag = event.target.getAttribute('data-flag');
-        
-        languageButton.querySelector('span').textContent = selectedLanguage;
-        languageFlag.src = selectedFlag;
-  
-        languageDropdown.classList.remove('show');
-        console.log('Language selected:', selectedLanguage);
-      }
-    });
-  
-    document.addEventListener('click', function (event) {
-      if (!languageButton.contains(event.target) && !languageDropdown.contains(event.target)) {
-        languageDropdown.classList.remove('show');
-      }
-    });
+  languageDropdown.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+      const selectedLanguage = event.target.getAttribute("data-lang");
+      const selectedFlag = event.target.getAttribute("data-flag");
+
+      languageButton.querySelector("span").textContent = selectedLanguage;
+      languageFlag.src = selectedFlag;
+
+      languageDropdown.classList.remove("show");
+      console.log("Language selected:", selectedLanguage);
+    }
   });
-  
+
+  document.addEventListener("click", function (event) {
+    if (
+      !languageButton.contains(event.target) &&
+      !languageDropdown.contains(event.target)
+    ) {
+      languageDropdown.classList.remove("show");
+    }
+  });
+});
+
+
+
+
+const searchInputEl = document.querySelector(".search-input");
+const dropdownWrapper = document.querySelector(".dropdown-wrapper");
+
+let isSearchOpened = false;
+
+
+searchInputEl.addEventListener('click', () => {
+    isSearchOpened = !isSearchOpened;
+
+    if (isSearchOpened) {
+        dropdownWrapper.classList.remove('hidden');
+        dropdownWrapper.classList.add('opacity-100');
+    } else {
+        dropdownWrapper.classList.add('hidden');
+        dropdownWrapper.classList.remove('opacity-100');
+    }
+});
